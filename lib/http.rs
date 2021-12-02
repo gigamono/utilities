7d6fc6dee0cde3448be1c74cebcf9eee4de1d4d7
Stephen Akinyemi <appcypher@outlook.com>
2021-12-02 15:56:50 +0100

    Remove custom response/request in favor of Hyper's
    We don't need them for now since request/response impl has been removed

--- lib/http.rs
+++ lib/http.rs
@@ -1,12 +1,8 @@
 // Copyright 2021 the Gigamono authors. All rights reserved. Apache 2.0 license.
 
 mod constants;
-mod request;
-mod response;
-mod parts;
+mod status_codes;
 pub mod utils;
 
 pub use constants::*;
-pub use request::*;
-pub use response::*;
-pub use parts::*;
+pub use status_codes::*;
--- lib/http/status_codes.rs
+++ lib/http/status_codes.rs
@@ -1,26 +1,7 @@
 // Copyright 2021 the Gigamono authors. All rights reserved. Apache 2.0 license.
 
 use serde::{Deserialize, Serialize};
-
 use strum_macros::Display;
-
-#[derive(Serialize, Deserialize)]
-pub enum Version {
-    Http09,
-    Http10,
-    Http11,
-    H2,
-    H3,
-}
-
-#[derive(Serialize, Deserialize, Default)]
-pub struct Uri {
-    pub scheme: Option<String>,
-    pub authority: Option<String>,
-    pub path: String,
-    pub query: Option<String>,
-}
-
 
 #[derive(Serialize, Deserialize, Debug, Display, Clone, Copy)]
 pub enum StatusCode {
@@ -40,11 +21,5 @@ impl StatusCode {
 impl Default for StatusCode {
     fn default() -> Self {
         Self::Ok
-    }
-}
-
-impl Default for Version {
-    fn default() -> Self {
-        Self::Http11
     }
 }
