ae87a8f711612b11fcee06b9932d939d37fcef00
Stephen Akinyemi <appcypher@outlook.com>
2021-12-28 11:20:23 +0100

    Add theme config

--- lib/config.rs
+++ lib/config.rs
@@ -1,9 +1,11 @@
 // Copyright 2021 the Gigamono authors. All rights reserved. Apache 2.0 license.
 
-mod permissions;
 mod config_gigamono;
+mod config_theme;
 pub mod constants;
 mod manifest_api;
+mod permissions;
 
 pub use config_gigamono::*;
+pub use config_theme::*;
 pub use manifest_api::*;
---
+++ lib/config/config_theme.rs
@@ -0,0 +1,21 @@
+// Copyright 2021 the Gigamono authors. All rights reserved. Apache 2.0 license.
+
+use crate::nested_struct;
+use crate::result::{Context, Result};
+use serde::Deserialize;
+
+nested_struct! {
+    ThemeConfig {
+        meta (Meta {
+            kind (String),
+            version (String),
+        }),
+
+    }
+}
+
+impl ThemeConfig {
+    pub fn try_from(config_str: &str) -> Result<Self> {
+        serde_yaml::from_str(&config_str).context("deserializing theme config")
+    }
+}
